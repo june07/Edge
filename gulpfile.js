ea80156b257370381c2697ab155117803ebbe16a
Will <will@onezerohosting.com>
2025-06-12 12:02:28 -0700

    .

--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,83 @@
-const {series, parallel, watch, src, dest} = require('gulp');
-const pump = require('pump');
-const fs = require('fs');
-const order = require('ordered-read-streams');
+const { series, parallel, watch, src, dest } = require('gulp')
+const pump = require('pump')
+const fs = require('fs')
+const order = require('ordered-read-streams')
 
 // gulp plugins and utils
-const livereload = require('gulp-livereload');
-const postcss = require('gulp-postcss');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
-const beeper = require('beeper');
-const zip = require('gulp-zip');
+const livereload = require('gulp-livereload')
+const postcss = require('gulp-postcss')
+const concat = require('gulp-concat')
+const uglify = require('gulp-uglify')
+const beeper = require('beeper')
+const zip = require('gulp-zip')
 
 // postcss plugins
-const easyimport = require('postcss-easy-import');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
+const easyimport = require('postcss-easy-import')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
 
 function serve(done) {
-    livereload.listen();
-    done();
+    livereload.listen()
+    done()
 }
 
 function handleError(done) {
     return function (err) {
         if (err) {
-            beeper();
+            beeper()
         }
-        return done(err);
-    };
+        return done(err)
+    }
 };
 
 function hbs(done) {
     pump([
         src(['*.hbs', 'partials/**/*.hbs']),
         livereload()
-    ], handleError(done));
+    ], handleError(done))
 }
 
 function css(done) {
     pump([
-        src('assets/css/screen.css', {sourcemaps: true}),
+        src('assets/css/screen.css', { sourcemaps: true }),
         postcss([
             easyimport,
             autoprefixer(),
             cssnano()
         ]),
-        dest('assets/built/', {sourcemaps: '.'}),
+        dest('assets/built/', { sourcemaps: '.' }),
         livereload()
-    ], handleError(done));
+    ], handleError(done))
 }
 
 function getJsFiles(version) {
     const jsFiles = [
         src(`node_modules/@tryghost/shared-theme-assets/assets/js/${version}/lib/**/*.js`),
         src(`node_modules/@tryghost/shared-theme-assets/assets/js/${version}/main.js`),
-    ];
+    ]
 
     if (fs.existsSync(`assets/js/lib`)) {
-        jsFiles.push(src(`assets/js/lib/*.js`));
+        jsFiles.push(src(`assets/js/lib/*.js`))
     }
 
-    jsFiles.push(src(`assets/js/main.js`));
+    jsFiles.push(src(`assets/js/main.js`))
 
-    return jsFiles;
+    return jsFiles
 }
 
 function js(done) {
     pump([
-        order(getJsFiles('v1'), {sourcemaps: true}),
+        order(getJsFiles('v1'), { sourcemaps: true }),
         concat('main.min.js'),
         uglify(),
-        dest('assets/built/', {sourcemaps: '.'}),
+        dest('assets/built/', { sourcemaps: '.' }),
         livereload()
-    ], handleError(done));
+    ], handleError(done))
 }
 
 function zipper(done) {
-    const filename = require('./package.json').name + '.zip';
+    const { name, version } = require('./package.json')
+    const filename = `${name}-${version}.zip`
 
     pump([
         src([
@@ -87,15 +88,15 @@ function zipper(done) {
         ]),
         zip(filename),
         dest('dist/')
-    ], handleError(done));
+    ], handleError(done))
 }
 
-const hbsWatcher = () => watch(['*.hbs', 'partials/**/*.hbs'], hbs);
-const cssWatcher = () => watch('assets/css/**/*.css', css);
-const jsWatcher = () => watch('assets/js/**/*.js', js);
-const watcher = parallel(hbsWatcher, cssWatcher, jsWatcher);
-const build = series(css, js);
+const hbsWatcher = () => watch(['*.hbs', 'partials/**/*.hbs'], hbs)
+const cssWatcher = () => watch('assets/css/**/*.css', css)
+const jsWatcher = () => watch('assets/js/**/*.js', js)
+const watcher = parallel(hbsWatcher, cssWatcher, jsWatcher)
+const build = series(css, js)
 
-exports.build = build;
-exports.zip = series(build, zipper);
-exports.default = series(build, serve, watcher);
+exports.build = build
+exports.zip = series(build, zipper)
+exports.default = series(build, serve, watcher)
